ae2b291cd1936d4bb7c21d0ea5350388ab264ca7
Pramatha Bhat <bhatpramatha@gmail.com>
2023-07-27 13:12:33 -0400

    added tires module

--- battery/spindlerBattery.py
+++ battery/spindlerBattery.py
@@ -5,7 +5,7 @@ class SpindlerBattery(Battery):
     def __init__(self, current_date, last_service_date):
         self.current_date = current_date 
         self.last_service_date = last_service_date
-        self.years = 2
+        self.years = 3
 
     def needs_service(self):
         battery_service_date = add_years(self.last_service_date, self.years); 
